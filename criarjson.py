f508c789a1bfed70b855d5cc13a0bce25074cde9
Allan Gomes Cuypers <allan.cuypers@hotmail.com>
2022-09-27 21:04:56 -0300

    novo projeto

---
+++ criarjson.py
@@ -0,0 +1,43 @@
+import json
+import random
+
+quantidadeTotalComandasPagas = random.randrange(10,2000)
+quantidadeTotalPessoasPagas = random.randrange(10,2000)
+
+MaximoId = int(input('Digite o numero de quantas filiais vai acrescentar:'))
+id = 1
+while MaximoId > 0:
+    quantidadeTotalComandasPagas = random.randrange(10,2000)
+    quantidadeTotalPessoasPagas = random.randrange(10,2000)
+    
+    resumoFinanceiro = {
+                "valVendaBrutaItens": 69945.18,
+                "valVendaBrutaAdicionais": 0,
+                "valVendaBruta": 69945.18,
+                "valCancelamentosItens": 60008.19,
+                "valCancelamentosAdicionais": 0,
+                "valCancelamentos": 60008.19,
+                "valVendaEfetiva": 129953.37,
+                "valDescontosItens": 83.2,
+                "valDescontosAdicionais": 495.57,
+                "valDescontos": 578.77,
+                "valVendaLiquida": 9358.22,
+                "quantidadeTotalComandasPagas": quantidadeTotalComandasPagas,
+                "quantidadeTotalPessoasPagas": quantidadeTotalPessoasPagas
+    }
+
+    dados =	{
+                "id": id,
+                "cnpj": "38.436.516/0001-56",
+                "nomeFantasia": "TESTE ENGENHARIA",
+                "resumoFinanceiro": resumoFinanceiro
+    }
+    
+    converter = json.dumps(dados, indent=4, separators=(',',':'))
+    # Mudei de W para a merda do A e começou a escrever correto
+    with open ("exemplo.json","a") as outfile:
+        outfile.write(converter)
+
+    MaximoId -= 1
+    id += 1
+
